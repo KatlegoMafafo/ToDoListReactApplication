2e30e7421db2e2adf6369cb43224c7fe71fe2cab
Sifiso Mahlangu <sifiso.mahlangu@reverside.co.za>
2018-05-25 09:52:53 +0200

    To to list React application still on progress

---
+++ todolistapplication/src/TodoItems.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import FlipMove from "react-flip-move";
+
+class TodoItems extends Component{
+    constructor(props){
+        super(props);
+
+        this.createTask = this.createTask.bind(this);
+    }
+    createTask(item){
+        return <li onClick={()=>this.delete(item.key)} 
+                      key={item.key}>{item.text}</li>
+    }
+
+    delete(key){
+        console.log("key is: "+ key);
+        this.props.delete(key);
+    }
+
+    render(){
+        var todoEntries = this.props.entries;
+        var listItems = todoEntries.map(this.createTask);
+
+        return(
+            <ul className="theList">
+            <FlipMove duration={150} easing="ease-out">
+              {listItems}
+              </FlipMove>
+            </ul>
+
+        );
+    }
+}
+
+export default TodoItems;
